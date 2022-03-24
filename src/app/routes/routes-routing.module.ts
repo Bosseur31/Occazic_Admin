aed63189afa904d342351a1f72e7d52ad54ac5d6
aymeric <aymeric.baptiste@gmail.com>
2022-03-24 14:52:38 +0100

    Minor correct

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { environment } from '@env/environment';
 
-import { AdminLayoutComponent } from '../theme/admin-layout/admin-layout.component';
-import { AuthLayoutComponent } from '../theme/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from '@theme/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from '@theme/auth-layout/auth-layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './sessions/login/login.component';
 import { RegisterComponent } from './sessions/register/register.component';
