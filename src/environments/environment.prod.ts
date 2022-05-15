b7b596536097cacdef3587173f0b2c3e114113c2
aymeric <aymeric.baptiste@gmail.com>
2022-05-15 18:02:47 +0200

    Prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: '',
-  apiURL: 'http://admin-occasion.aymeric-mai.fr:3000',
+  apiURL: 'http://admin-occasion.prozic.com:3000',
   useHash: false,
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   baseUrl: '',
-  apiURL: 'http://admin-occasion.aymeric-mai.fr:3000',
+  apiURL: 'http://admin-occasion.prozic.com:3000',
   useHash: false,
 };
 
